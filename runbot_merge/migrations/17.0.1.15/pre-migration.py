8c70e44ae908939c96fd3d9485573648d85dcd7c
Xavier Morel <xmo@odoo.com>
2024-11-21 15:47:09 +0100

    [IMP] runbot)merge: 17.0 migration
    `runbot_merge.patch` is a mail.thread model added since the last
    migration attempt, see 36786d51c8bc85eeeca9340740db4626ad239bf0

--- runbot_merge/migrations/17.0.1.15/pre-migration.py
+++ runbot_merge/migrations/17.0.1.15/pre-migration.py
@@ -3,5 +3,6 @@ from odoo.upgrade import util
 def migrate(cr, _version):
     util.remove_field(cr, "res.partner", "message_main_attachment_id")
     util.remove_field(cr, "runbot_merge.batch", "message_main_attachment_id")
+    util.remove_field(cr, "runbot_merge.patch", "message_main_attachment_id")
     util.remove_field(cr, "runbot_merge.pull_requests", "message_main_attachment_id")
     util.remove_field(cr, "runbot_merge.pull_requests.feedback.template", "message_main_attachment_id")
