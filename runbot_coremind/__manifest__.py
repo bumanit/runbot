3eee1f472a2159826207bfb0a2249dcccc20d4e9
uuganbat <uuganbat.be@gmail.com>
2025-01-02 09:57:32 +0800

    [ADD] runbot_coremind

---
+++ runbot_coremind/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "runbot coremind",
+    "summary": "Runbot Coremind",
+    "description": "Runbot Coremind",
+    "author": "Coremind LLC",
+    "website": "http://runbot.coremind.mn",
+    "category": "Website",
+    "version": "1.0",
+    "depends": ["runbot"],
+    "demo": [
+        "data/data.xml",
+    ],
+    "license": "LGPL-3",
+}
